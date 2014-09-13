7d817ddea8eb7edb703c01bd8b3d3b4deac29344
Sergio <asergiop21@hotmail.com>
2014-09-13 00:58:59 -0300

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 root = "/var/www/project/current"
 working_directory  root
-pid "#{root}/tmp/pids/unicorn.pid"
+pid "/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
