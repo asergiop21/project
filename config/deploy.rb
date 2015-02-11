b3fa07cf9311d4e7a1667b1734bcb35a53cef5bb
Sergio <asergiop21@hotmail.com>
2015-02-11 16:17:49 -0300

    cambio server

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'project'
-set :repo_url, 'git@github.com:asergiop21/project_libreria.git'
+set :repo_url, 'git@github.com:asergiop21/project.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
@@ -13,7 +13,7 @@ set :repo_url, 'git@github.com:asergiop21/project_libreria.git'
 # Default value for :scm is :git
  set :scm, :git
 
- set :rbenv_ruby, '2.1.4'
+ set :rbenv_ruby, '2.1.2'
 # Default value for :format is :pretty
 # set :format, :pretty
 
