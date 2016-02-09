5ded8c2789c5c98fb3c85ebf42df40c662d86c4e
Sergio <asergiop21@gmail.com>
2016-02-09 12:15:29 -0300

    cambio de server

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,12 +8,14 @@ set :repo_url, 'git@github.com:asergiop21/project.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
- set :deploy_to, '/var/www/project_libreria'
+#set :deploy_to, '/var/www/project_libreria'
+set :deploy_to, '/opt/mercurio'
 
 # Default value for :scm is :git
- set :scm, :git
+set :scm, :git
 
- set :rbenv_ruby, '2.2.0'
+#set :rbenv_ruby, '2.2.0'
+set :rbenv_ruby, '2.2.4'
 # Default value for :format is :pretty
 # set :format, :pretty
 
@@ -56,4 +58,15 @@ namespace :deploy do
     end
   end
 
+  # # and run 'cap production deploy:seed' to seed your database
+  desc 'Runs rake db:seed'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
 root = "/var/www/project_libreria/current"
 working_directory  root
-pid "var/www/project_libreria/shared/pids/unicorn.pid"
+pid "/var/www/project_libreria/shared/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.project.sock"
+listen "/tmp/unicorn.project_libreria.sock"
 worker_processes 1
 timeout 600
 
